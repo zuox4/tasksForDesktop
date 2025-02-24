bf44ab4452a79a94d37a63d22959ffea0badfa09
zuox4 <kiryxa97g@gmail.com>
2025-02-24 23:41:48 +0300

    121

--- tasksForDesktop/src/components/ModalWindow.jsx
+++ tasksForDesktop/src/components/ModalWindow.jsx
@@ -1,8 +1,10 @@
-export const ModalWindow = () =>{
-    return(
-        <div style={{display:'',  height:'100%', width:'100vw', backgroundColor:'#ff00005e', position:'absolute', bottom:'0', left:0}}>
-            <div style={{height:'500px', width:'500px', background:'white', margin:'auto', }}>
+export const ModalWindow = ({ children }) =>{
 
+    return(
+        <div style={{display:'flex',  height:'100%', width:'100vw', backgroundColor:'rgba(51,51,51,0.71)', position:'fixed', bottom:'0', left:0}}>
+            <div style={{height:'500px', width:'500px', background:'white', margin:'auto', }}>
+                <div>Закрыть</div>
+                {children}
             </div>
         </div>
     )
--- tasksForDesktop/src/components/ShopDashboard.jsx
+++ tasksForDesktop/src/components/ShopDashboard.jsx
@@ -1,8 +1,10 @@
 import { ShopItem } from "./ShopItem"
+import {ModalWindow} from "./ModalWindow.jsx";
 
 export const ShopDashboard = () =>{
     return(
         <div style={{display:'flex', flexWrap:'wrap', justifyContent:'space-between', rowGap:'40px', marginTop:'30px', columnGap:'30px'}}>
+
             <ShopItem/>
             <ShopItem/>
             <ShopItem/>
